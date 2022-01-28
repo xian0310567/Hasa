73b4fcf5185215087087a0e5fc5052a7f2ab3d70
xian0310567 <xian0310567@naver.com>
2022-01-28 20:47:26 +0900

    feat: auth Get 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth/auth.controller';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AuthController],
+  providers: [PrismaService],
 })
 export class AppModule {}
