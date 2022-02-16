c4a07faf1f2472d080a1ce951e61e494968072a6
xian0310567 <xian0310567@naver.com>
2022-02-16 19:44:45 +0900

    feat: redirect 수정

---
+++ client/src/Router.ts
@@ -0,0 +1,7 @@
+import Home from './components/Home.svelte';
+
+const routers = {
+    '/': Home,
+}
+
+export default routers;
---
+++ client/src/components/Home.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<header>
+    <img id="logo" src="images/logo-kasa.svg" alt="logo for kasa">
+    <div id="information">
+        <img src="images/alert.png" alt="" width="25px">
+        <img src="images/user.png" alt="" width="23px">
+    </div>
+</header>
+
+<style>
+    header {
+        box-sizing : border-box;
+    }
+    #logo {
+        margin : 10px;
+    }
+    #information {
+        float : right;
+        margin : 10px;
+        opacity : 0.8;
+    }
+    #information img:nth-child(2) {
+        margin-left : 10px;
+    }
+</style>
