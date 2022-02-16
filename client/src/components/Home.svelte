a41cc07e20cd5ad3fa8f065acf0a1fbf19c9218b
xian0310567 <xian0310567@naver.com>
2022-02-16 20:32:13 +0900

    feat: 예치금 및 사용자 명 store 추가

--- client/src/components/Home.svelte
+++ client/src/components/Home.svelte
@@ -1,15 +1,31 @@
 <script>
+    import { userName, propPrice } from "../store";
 </script>
 
+
+<!-- body -->
 <header>
-    <img id="logo" src="images/logo-kasa.svg" alt="logo for kasa">
+    <img id="logo" src="images/logo-kasa.svg" alt="logoForKasa">
     <div id="information">
         <img src="images/alert.png" alt="" width="25px">
         <img src="images/user.png" alt="" width="23px">
     </div>
 </header>
 
+<div id="invest">
+    <h3 class="subTitle">{$userName} 님의 투자</h3>
+    <div id="propBox">
+        <span>예치금</span>
+        <span>{$propPrice}원 <img src="images/right-arrow.png" alt="예치금 바로가기" width = "10px"></span>
+    </div>
+</div>
+
+
+<!-- body end -->
+
 <style>
+
+    /* header */
     header {
         box-sizing : border-box;
     }
@@ -23,5 +39,29 @@
     }
     #information img:nth-child(2) {
         margin-left : 10px;
+    }
+
+    /* invest */
+    #invest {
+        padding : 0 10px;
+        /* box-sizing : border-box; */
+    }
+    .subTitle {
+        font-weight : bold;
+        margin-bottom : 15px;
+    }
+    #propBox {
+        box-shadow : 0px 0px 5px #d5d5d5;
+        padding : 14px;
+    }
+    #propBox span {
+        opacity : 0.7;
+        font-size : 14px;
+        font-weight : bold;
+    }
+
+    #propBox span:nth-child(2) {
+        float : right;
+        opacity : 1;
     }
 </style>
---
+++ client/src/store.ts
@@ -0,0 +1,8 @@
+import { writable } from "svelte/store";
+
+// 사용자 명
+export let userName = writable('이정한');
+
+// 예치금
+export let propPrice = writable('0');
+
