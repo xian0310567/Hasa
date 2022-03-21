d3b73aecbc72d40d205cbdfaa3b23d844a87b5a3
xian0310567 <xian0310567@naver.com>
2022-03-21 13:03:50 +0900

    feat: 계좌 잔액 입금 및 출금 기능 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,17 +3,21 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { AccountController } from './account/account.controller';
+import { AccountService } from './account/account.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [
-    UserController
+    UserController,
+    AccountController
   ],
   providers: [
     PrismaService,
-    UserService
+    UserService,
+    AccountService
   ],
 })
 export class AppModule {}
